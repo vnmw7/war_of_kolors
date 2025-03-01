fb29f0e7b0956844f5383590574f07089ea9d0a8
vincentm.work.2023@gmail.com <vincentm.work.2023@gmail.com>
2025-03-01 20:40:58 +0800

    eslint fixing

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="w-screen h-screen flex flex-wrap">
       <ConnectWallet setWalletConnected={setWalletConnected} />
-      {/* {walletConnect && <CheckBalance />} */}
+      {walletConnect && <CheckBalance />}
     </main>
   );
 }
